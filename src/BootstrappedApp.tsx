7c965606134f313d84de7be4f516502866cab564
Novel Yourdhan Rehandhika <87990940+novelyr@users.noreply.github.com>
2025-01-24 15:41:42 +0700

    Revert "tugas 23"

--- src/BootstrappedApp.tsx
+++ src/BootstrappedApp.tsx
@@ -1,12 +1,6 @@
-import { store } from '@/services/redux';
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
 import App from './App';
 
 export const BootstrappedApp: React.FC = () => {
-  return (
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
-  );
+  return <App />;
 };
